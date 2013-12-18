df265381452e561e765b06abb4c15345a44882d8
Steffen Herrdum <steffen.herrdum@uni-ulm.de>
2013-12-18 16:12:48 +0100

    added receiver for forecast data

--- src/de/uniulm/bagception/weatherforecasttester/MainActivity.java
+++ src/de/uniulm/bagception/weatherforecasttester/MainActivity.java
@@ -1,6 +1,11 @@
 package de.uniulm.bagception.weatherforecasttester;
 
-import de.uniulm.bagception.weatherforecasttester.MyResultReceiver.Receiver;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import de.uniulm.bagception.intentservicecommunication.MyResultReceiver;
+import de.uniulm.bagception.intentservicecommunication.MyResultReceiver.Receiver;
 import android.os.Bundle;
 import android.os.Handler;
 import android.app.Activity;
@@ -11,13 +16,18 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends Activity implements Receiver{
 
-    public MyResultReceiver mReceiver; 
+	private MyResultReceiver mResultreceiver;
+	
     private Button mGetWeatherForecastButton;
     private EditText mLatEditText;
     private EditText mLongEditText;
+    private EditText mAnswerBox;
+    
+    private JSONObject result;
  
 	
 	@Override
@@ -25,25 +35,30 @@ public class MainActivity extends Activity implements Receiver{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		mResultreceiver = new MyResultReceiver(new Handler());
+		mResultreceiver.setReceiver(this);
+		
 		this.mLatEditText = (EditText) findViewById(R.id.latEditText);
 		this.mLongEditText = (EditText) findViewById(R.id.longEditText);
+		this.mAnswerBox = (EditText) findViewById(R.id.answerBox);
 		this.mGetWeatherForecastButton = (Button) findViewById(R.id.getWeatherForeCast_btn);
 		mGetWeatherForecastButton.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				// TODO: Get text from editViews
-				double lat1 = 40.0;
-				double long1 = 10.0;
-//				Intent i = new Intent(this, MyIntentService.class);
-				Intent i = getPackageManager().getLaunchIntentForPackage("de.uniulm.bagception.weatherforecast");
-                i.putExtra("nameTag","sohail" );
-                i.putExtra("receiverTag", mReceiver);
-                startActivity(i);
+//				double lat1 = Double.parseDouble(mLatEditText.getText().toString());
+//				double long1 = Double.parseDouble(mLongEditText.getText().toString());
+				
+				String serviceString = "de.uniulm.bagception.weatherforecast.WeatherForecastService";
+				
+				Intent i = new Intent(serviceString);
+				i.putExtra("origin","A" );
+//				i.putExtra("lat1", lat1);
+//				i.putExtra("long1", long1);
+				i.putExtra("receiverTag", mResultreceiver);
+				startService(i);
 			}
 		});
-		
-		mReceiver = new MyResultReceiver(new Handler());
-        mReceiver.setReceiver(this);
 	}
 	
 
@@ -54,9 +69,21 @@ public class MainActivity extends Activity implements Receiver{
 		return true;
 	}
 
+
 	@Override
 	public void onReceiveResult(int resultCode, Bundle resultData) {
-		Log.d("hahahaha", "it works!!!!!!!!!!!!!!!!!!!");
+		String s = resultData.getString("payload");
+		
+		try {
+			JSONObject object = new JSONObject(s);
+			setWeatherData(object);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
-
+	
+	public void setWeatherData(JSONObject result) throws JSONException{
+		mAnswerBox.setText(result.toString());
+	}
 }
